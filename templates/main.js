4e8413c907905f5299da14a138b2988f7cade685
soha <soy2302ten@gmail.com>
2021-11-06 11:15:22 +0900

    버튼 열고 닫기 기능

--- templates/main.js
+++ templates/main.js
@@ -6,3 +6,14 @@ function initMap() {
     zoom: 8,
   });
 }
+
+function openClose() {
+  // id 값 post-box의 display 값이 block 이면
+  if ($('#posting-box').css('display') == 'block') {
+      // post-box를 가리고
+      $('#posting-box').hide();
+  } else {
+      // 아니면 post-box를 보여주기
+      $('#posting-box').show();
+  }
+}
