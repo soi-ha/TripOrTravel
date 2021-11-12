7751b0ebf41ccb4789fe3b06bc2a0a85f1d05392
soha <soy2302ten@gmail.com>
2021-11-13 01:50:44 +0900

    Create js and app

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/trip', methods=['POST'])
+def post_article():
+    # 클라이언트로부터 데이터를 받기
+    writer_receive = request.form['writer_give']
+    date_receive = request.form['date_give']
+    place_receive = request.form['place_give']
+    img_receive = request.form['img_give']
+    content_receive = request.form['content_give']
+
+    article = {'writer': writer_receive, 'date': date_receive, 'place': place_receive,
+               'img': img_receive, 'content': content_receive}
+
+    #  mongoDB에 데이터를 넣기
+    db.trip.insert_one(article)
+    # 성공 여부 & 성공 메시지 반환하기
+    return jsonify({'result': 'success'})
+
+
+@app.route('/memo', methods=['GET'])
+def read_articles():
+    # mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기 (Read)
+    result = list(db.trip.find({}, {'_id': 0}))
+    # articles라는 키 값으로 article 정보 보내주기
+    return jsonify({'result': 'success', 'articles': result})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
--- templates/main.js
+++ templates/main.js
@@ -1,21 +1,84 @@
+$(document).ready(function () {
+    $("#card-bundle").html("");
+    showArticles();
+});
+
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8,
+    });
 }
 
 function openClose() {
-  // id 값 post-box의 display 값이 block 이면
-  if ($('#posting-box').css('display') == 'block') {
-    // post-box를 가리고
-    $('#posting-box').hide();
-  } else {
-    // 아니면 post-box를 보여주기
-    $('#posting-box').show();
-  }
+    // id 값 post-box의 display 값이 block 이면
+    if ($('#posting-box').css('display') === 'block') {
+        // post-box를 가리고
+        $('#posting-box').hide();
+    } else {
+        // 아니면 post-box를 보여주기
+        $('#posting-box').show();
+    }
+}
+
+function postArticle() {
+    const writer = $("#post-writer").val();
+    const date = $("#post-trip-date").val();
+    const place = $("#post-trip-place").val();
+    const img = $("#post-img").val();
+    const content = $("#post-trip-content").val();
+
+    // POST 방식으로 카드 생성 요청하기
+    $.ajax({
+        type: "POST", // POST 방식으로 요청하겠다.
+        url: "/trip", // /memo라는 url에 요청하겠다.
+        data: {
+            writer_give: writer,
+            date_give: date,
+            place_give: place,
+            img_give: img,
+            content_give: content
+        }, // 데이터를 주는 방법
+        success: function (response) { // 성공하면
+            if (response["result"] === "success") {
+                alert("포스팅 성공!");
+                // 3. 성공 시 페이지 새로고침하기
+                window.location.reload();
+            } else {
+                alert("서버 오류!");
+            }
+        }
+    })
+}
+
+function showArticles() {
+    $.ajax({
+        type: "GET",
+        url: "/trip",
+        data: {},
+        success: function (response) {
+            const articles = response["articles"];
+            console.log(articles);
+            for (let i = 0; i < articles.length; i++) {
+                makeCard(articles[i]["writer"], articles[i]["img"], articles[i]["date"], articles[i]["place"], articles[i]["content"]);
+            }
+        }
+    })
+}
+
+function makeCard(writer, img, date, place, content) {
+    const tempHtml = `<div class="cards-box">
+                      <div class="card">
+                        <span class="card-writer">Writer ${writer}</span>
+                        <img class="card-img" src="${img}">
+                        <span class="card-trip-date">여행 날짜: ${date}</span>
+                        <span class="card-trip-place">여행 장소: ${place}</span>
+                        <span class="card-trip-content">${content}</span>
+                      </div>
+                    </div>`;
+    $("#card-bundle").append(tempHtml);
 }
 
 // 더보기 버튼 js
@@ -28,6 +91,6 @@ function openClose() {
 //     title.classList.toggle('clamp');
 // })
 
-/* morebutton이 클릭이 되면 morebutton의 클래스를 클릭이
-됐는지 안됐는지 토글을 함. title의 클래스를 클램프 하라는지 안하라는지 
-토글을 할 것임 */ 
+// morebutton이 클릭이 되면 morebutton의 클래스를 클릭이
+//됐는지 안됐는지 토글을 함. title의 클래스를 클램프 하라는지 안하라는지 
+//토글을 할 것임
